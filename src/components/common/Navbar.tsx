39d2cf8e0a9e426c5acbaab3d9cccf84dd8e4c2f
benbeckman2001 <trungthekop123@gmail.com>
2024-10-01 10:52:40 +0700

    Update code

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 const Navbar = async () => {
   const router = useRouter();
   const authUser = await currentUser();
+  const name = 'ffffffff';
 
   return (
     <header className='fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between'>
--- src/store.tsx
+++ src/store.tsx
@@ -1,32 +1,29 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
 export interface Option {
-  value: string
-  label: string
-  disable?: boolean
+  value: string;
+  label: string;
+  disable?: boolean;
   /** fixed option that can't be removed. */
-  fixed?: boolean
+  fixed?: boolean;
   /** Group the options by providing key. */
-  [key: string]: string | boolean | undefined
+  [key: string]: string | boolean | undefined;
 }
 
 type FuzzieStore = {
-  googleFile: any
-  setGoogleFile: (googleFile: any) => void
-  slackChannels: Option[]
-  setSlackChannels: (slackChannels: Option[]) => void
-  selectedSlackChannels: Option[]
-  setSelectedSlackChannels: (selectedSlackChannels: Option[]) => void
-}
+  googleFile: any;
+  setGoogleFile: (googleFile: any) => void;
+  slackChannels: Option[];
+  setSlackChannels: (slackChannels: Option[]) => void;
+  selectedSlackChannels: Option[];
+  setSelectedSlackChannels: (selectedSlackChannels: Option[]) => void;
+};
 
-export const useFuzzieStore = create<FuzzieStore>()((set) => ({
+export const useFuzzieStore = create<FuzzieStore>()(set => ({
   googleFile: {},
   setGoogleFile: (googleFile: any) => set({ googleFile }),
   slackChannels: [],
   setSlackChannels: (slackChannels: Option[]) => set({ slackChannels }),
   selectedSlackChannels: [],
-  setSelectedSlackChannels: (selectedSlackChannels: Option[]) =>
-    set({ selectedSlackChannels }),
-}))
-
-
+  setSelectedSlackChannels: (selectedSlackChannels: Option[]) => set({ selectedSlackChannels })
+}));
